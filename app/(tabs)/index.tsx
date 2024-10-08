0f116778211d341dff18969eeaba0069cee1eb01
Awais Shahid <i212764@nu.edu.pk>
2024-10-08 07:40:55 +0500

    firstCommit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Platform, StyleSheet } from "react-native";
+import React from "react";
+
+export default function TabOneScreen() {
+  return (
+    <View>
+
+    </View>
+  );
+}
