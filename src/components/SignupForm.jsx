462f1c5ce963c7f9bd8fdc4fe94a08e2d89b9bf2
Awais Shahid <i212764@nu.edu.pk>
2025-03-09 13:08:30 +0500

    getting errror on viewbox being a weird number for green tick or red cross

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -575,7 +575,7 @@ const SignupForm = ({
                           xmlns="http://www.w3.org/2000/svg"
                           className="h-5 w-5"
                           fill="none"
-                          viewBox="0 0 24 24"
+                          viewBox="0 24 24"
                           stroke="currentColor"
                         >
                           <path
@@ -619,7 +619,7 @@ const SignupForm = ({
                           xmlns="http://www.w3.org/2000/svg"
                           className="h-5 w-5"
                           fill="none"
-                          viewBox="0 0 24 24"
+                          viewBox="0 24 24"
                           stroke="currentColor"
                         >
                           <path
