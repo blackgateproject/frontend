23aef8d756c506a5203efb34b78ce7bd398012f2
Awais Shahid <i212764@nu.edu.pk>
2025-04-16 02:33:14 +0500

    disabled veramo clientside, got admin requests to work, dashboard now queries on DID

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { VeramoProvider } from "@veramo-community/veramo-react";
+// import { VeramoProvider } from "@veramo-community/veramo-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { localAgent } from "./veramo/create-agent";
+// import { localAgent } from "./veramo/create-agent";
 
 const queryClient = new QueryClient();
 
@@ -12,9 +12,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       {/* <VeramoProvider > */}
-      <VeramoProvider agents={[localAgent]}>
+      {/* <VeramoProvider agents={[localAgent]}> */}
         <App />
-      </VeramoProvider>
+      {/* </VeramoProvider> */}
     </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/user/GrafanaDashboard.jsx
+++ src/pages/user/GrafanaDashboard.jsx
@@ -11,7 +11,8 @@ const GrafanaDashboard = () => {
   // const grafanaUrl = `http://localhost:3000/public-dashboards/5c6de844c29a4783a657cb4d8021fe44?orgId=2`
   // Awais grafana
   // const grafanaUrl = `http://${grafanaHost}:${grafanaPort}/public-dashboards/${grafanaDashboard}`;
-  const did = localStorage.getItem("did");
+  
+  const did = JSON.parse(localStorage.getItem("verifiable_credential"))?.credential?.credentialSubject?.did || "";
   const grafanaUrl = `${grafanaURL}/d/aeg8k8xe3vmrkf/blackgate?orgId=1&from=now-6h&to=now&timezone=browser&var-query0=${did}&refresh=5s&kiosk`;
 
   return (
